d6b0eed950e3c77e35e4ddf1270c456a49a3e4fc
Kars <karssloeserwij@gmail.com>
2022-06-06 10:10:03 -0400

    Seperate Menu Button Comp and create Http comp

--- src/components/TestComp.jsx
+++ src/components/TestComp.jsx
@@ -1,79 +1,169 @@
 
 // @ts-nocheck
-import { useState, useEffect, useRef } from "../fre-godot";
-import LabelButton from "./LabelButton";
-import CLOSEICON from "res://cross.png";
-import SECONDSCENE from "res://second.tscn";
+import { useState, useRef, useEffect } from "../fre-godot";
 
-function MenuButton({ options = [], text = 'menu', }) {
-   const ref = useRef(null)
+function Test({ on_load_posts }) {
+   const [text, setText] = useState(1);
 
-   useEffect(() => {
-      const menu = ref.current;
-      options.forEach((el) => {
-         if (typeof el === 'object') {
-            menu.get_popup().add_item(el.name)
-         } else {
-            menu.get_popup().add_item(el)
+
+   function makeRequest() {
+      return new Promise((res, rej) => {
+         const http = new godot.HTTPClient()
+         // This method works for both anyway
+         let rb = new godot.PoolByteArray() // Array that will hold the data.
+
+         function checkConnection() {
+            while (
+               http.get_status() == godot.HTTPClient.STATUS_CONNECTING
+               || http.get_status() == godot.HTTPClient.STATUS_RESOLVING
+            ) {
+               http.poll()
+               //console.log("Connecting...")
+            }
+
+         }
+
+         function doRequest() {
+            while (
+               http.get_status() == godot.HTTPClient.STATUS_REQUESTING
+            ) {
+               http.poll()
+               //console.log("Requesting...")
+            }
+         }
+
+         function doGetChunck() {
+            while (
+               http.get_status() == godot.HTTPClient.STATUS_BODY
+            ) {
+               // While there is body left to be read
+               http.poll()
+               // Get a chunk.
+               var chunk = http.read_response_body_chunk()
+
+               if (chunk.size() == 0) {
+
+               } else {
+                  rb.append_array(chunk) // Append to read buffer.
+               }
+            }
+         }
+
+         try {
+            var err = http.connect_to_host('jsonplaceholder.typicode.com', 80)
+            if (err) { throw new Error(err) }
+            checkConnection();
+            console.log(http.get_status() == godot.HTTPClient.STATUS_CONNECTED)
+
+            //Some headers
+            var headers = [
+               "User-Agent: Pirulo/1.0 (Godot-Fre)",
+               "Accept: */*"
+            ]
+
+            err = http.request(godot.HTTPClient.METHOD_GET, "/posts", headers) //Request a page from the site(this one was chunked..)
+            if (err) { throw new Error(err) }
+            doRequest();
+
+
+            console.log("response? ", http.has_response()) // Site might not have a response.
+
+            if (http.has_response()) {
+               //If there is a response...
+               headers = http.get_response_headers_as_dictionary() // Get response headers.
+               console.log("code: ", http.get_response_code()) // Show response code.
+               console.log("**headers:\\n", JSON.stringify(headers)) // Show headers.
+
+               if (http.is_response_chunked()) {
+                  // Does it use chunks?
+                  console.log("Response is Chunked!")
+               } else {
+                  // Or just plain Content-Length
+                  var bl = http.get_response_body_length()
+                  console.log("Response Length: ", bl)
+               }
+               doGetChunck();
+               if (err) { throw new Error(err) }
+
+               console.log("bytes got: ", rb.size())
+               var t = rb.get_string_from_ascii()
+               //setText(t)
+               console.log("!!!!!")
+               res(t)
+               return;
+            }
+
+         } catch (e) {
+            console.log(e)
+            rej(e);
+            return;
          }
       })
+   }
 
-      menu.get_popup().connect('index_pressed', (i) => {
-         options?.[i]?.onPressed ? options[i].onPressed() : null
-      })
+   useEffect(() => {
 
-      return () => {
-         menu.get_popup().clear()
-      }
+      makeRequest()
+         .then((res) => {
+            setText(res)
+            on_load_posts(JSON.parse(res))
+         }).catch((err) => {
+            console.log(err)
+         })
 
    }
-      , [options])
-
+      , [])
 
 
 
    return (
-      <menubutton switch_on_hover={true} ref={ref} text={text} />
+      <label text={`${text}`} />
    )
-
 }
 
 export default function TestComp() {
    const rootRef = useRef(null)
+   const [posts, setPosts] = useState([])
+   const [hasReceivedPosts, setHasReceivedPosts] = useState(false)
 
-   const [options, setOptions] = useState( [
-      {
-         name: "Test",
-      },
-      {
-         name: "Test",
-      },
-      {
-         name: "Test",
-      },
-   ])
 
-   function handleClick() {
-      rootRef.current.get_tree().change_scene_to(SECONDSCENE)
+
+   function receivedPosts(newPosts) {
+      console.log('---- received posts----')
+       console.log(newPosts.id)
+       console.log([...posts, newPosts])
+       console.log([...posts, newPosts])
+       setPosts(newPosts)
+       console.log(setHasReceivedPosts)
+       setHasReceivedPosts(true)
    }
 
-   function addOption() {
-      setOptions([...options, {name:'button test', onPressed: handleClick}])
-   }
+   useEffect(() => {
+      console.log('posts')
+      console.log(posts)
+   },[posts])
 
+   useEffect(() => {
+      console.log('hasReceivedPosts')
+      console.log(hasReceivedPosts)
+   },[hasReceivedPosts])
+   
    return (
       <panelcontainer anchor={15} rect_min_size={new godot.Vector2(200, 200)}>
-         <panel ref={rootRef}>
-            <vbox anchor={15}>
-               <panelcontainer>
-               </panelcontainer>
-               <button text={'add option'} onPressed={addOption} />
-               <hbox>
-                  <MenuButton
-                     text={'Test'}
-                     options={options}
-                  />
-               </hbox>
+         <panel anchor={15} >
+               
+            <vbox >
+
+               
+               {!hasReceivedPosts && 
+               <Test on_load_posts={receivedPosts} />
+               }
+               {posts.map((post, i) => (
+            
+                  <label key={post.id} text={post.title} />
+                  
+               ))}
+
             </vbox>
          </panel>
       </panelcontainer>
