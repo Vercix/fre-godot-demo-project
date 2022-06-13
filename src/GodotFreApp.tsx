45ad1d6085b0942d3435dbe3bef0a583aa45e5e5
VercingetorixChief <karssloeserwij@gmail.com>
2022-06-13 18:45:27 -0400

    initial theme implementation

--- src/GodotFreApp.tsx
+++ src/GodotFreApp.tsx
@@ -1,35 +1,63 @@
 import { render } from "./fre-godot";
 
+import App from "./classes/BaseApp";
+import Calculator from './components/Calculator'
+import ToDoList from './components/ToDoList'
+
+const rootTheme = {
+	VBoxContainer:
+	{
+		constant: {
+			seperation: 1
+		}
+	},
+	Label:
+	{
+		color: {
+			font_color: new godot.Color(0, 1, 0)
+		}
+	},
+	Button:
+	{
+		color: {
+			font_color: new godot.Color(1, 0, 0),
+			font_color_hover: new godot.Color(0, 0, 1),
+			font_color_pressed: new godot.Color(0, 1, 1)
+		}
+	}
+}
 
 export default class GodotFreApp extends godot.Control {
-	app : string;
-	
-	
+	app: string;
+
 	constructor() {
-		super();	
+		super();
 	}
-	
-	async _enter_tree() {
-		console.log("~~~~~~~~~~~~~~~~~~~~~~~");
+
+	_enter_tree() {
 	}
-	
+
 	// Called when the node enters the scene tree for the first time.
 	async _ready() {
 		console.log("_____Ready______");
-		var test = (await import(`res://dist/components/${this.app}.jsx`))
-		render(<test.default/>, this);
+		render(
+			<App
+				anchor={15}
+				theme={rootTheme}
+			>
+				<Calculator />
+				<ToDoList />
+			</App>,
+			this
+		);
 	}
-	
+
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	_process(delta) {
-		
+
 	}
-	
-	
+
 }
 
 //godot
 godot.register_class(GodotFreApp, 'GodotFreApp');
-godot.set_script_icon(GodotFreApp, 'res://cross.png');
-godot.register_property(GodotFreApp, 'app', 'ToDoList');
-
