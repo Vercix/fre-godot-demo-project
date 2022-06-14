6c2fe7983dced2ee3eff5841bbc8c2a5b134bee3
Kars <karssloeserwij@gmail.com>
2022-06-14 07:43:07 -0400

    Remove num2 and allow substraction

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -2,16 +2,16 @@ import { useState } from "../fre-godot";
 
 export default function Counter() {
     const [num, setNum] = useState(0)
-    const [num2, setNum2] = useState(0)
 
     return (
-        <vbox>
-            <label text={String(num)} />
-            <label text={String(num2)} />
-            <button text={'add'} onPressed={() => {
-                setNum(c => c + 1)
-                setNum2(c => c - 1)
+        <hbox>
+            <button text={'-'} on_pressed={() => {
+                setNum(num - 1)
             }} />
-        </vbox>
+            <label text={String(num)} />
+            <button text={'+'} on_pressed={() => {
+                setNum(num + 1)
+            }} />
+        </hbox>
     )
 }
