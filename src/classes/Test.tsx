2e28e341de3f9886e8c4fc744a503b824383f551
Kars <karssloeserwij@gmail.com>
2022-06-15 20:05:51 -0400

    clean up

--- src/classes/Test.tsx
+++ src/classes/Test.tsx
@@ -1,43 +1,27 @@
 import { IFiber } from 'fre-godot'
-import { render } from '../fre-godot'
 
-
-interface FreNode {
-    _render: () => IFiber
-}
-
-export default class TestClass extends godot.Control implements FreNode {
+export default class TestClass extends godot.Control {
 
     //fre
     fiber: IFiber = null;
-    children = [];
-    renderOnEnter = false;
 
     constructor() {
         super();
     }
 
     _enter_tree() {
-        if(this.renderOnEnter){
-            render(this._render(), this)
-        }
     }
 
-    // Called when the node enters the scene tree for the first time.
     _ready() {
-
     }
 
     _render() {
         return (
             <vbox>
                 <label text="Test Class Comp" ></label>
-                <label text="Test Class Comp" ></label>
-                {this.children}
+                {this.fiber.props.children}
             </vbox>
         )
     }
 
 }
-
-godot.register_property(TestClass, 'renderOnEnter', false);
